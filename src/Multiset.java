83d9af97629f76dcb79cc6e45c64675e9adbe947
jihyuky <jihyuky@gmail.com>
2023-09-18 16:36:41 -0400

    constructor

---
+++ src/Multiset.java
@@ -0,0 +1,7 @@
+import java.lang.Object.org.omg.CORBA.Any;
+
+public class Multiset {
+    public Boolean add(int item){
+
+    }
+}
--- src/Tree.java
+++ src/Tree.java
@@ -1,2 +1,15 @@
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.Random;
+
 public class Tree {
+    private int root;  //TODO: Change this to Optional[Any]
+    private ArrayList<Tree> subTrees;
+    private String s;
+
+    public Tree(int root, ArrayList<Tree> subTrees) {
+        this.root = root;
+        this.subTrees = subTrees;
+    }
 }
+
